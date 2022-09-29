bb206513dcc99e364d4a8dc2afc42f2585b3057e
Trina <92634994+trinayau@users.noreply.github.com>
2022-09-29 10:50:17 +0100

    added default argument 'interactive' and executeInteractively method

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
         double[] leftVals = {100.0d, 25.0d, 225.0d, 11.0d};
@@ -14,11 +16,32 @@ public class Main {
             for (double currentResult : results)
                 System.out.println(currentResult);
 
+        } else if(args.length ==1 && args[0].equals("interactive")) {
+            executeInteractively();
         } else if(args.length == 3){
             handleCommandLine(args);
         } else {
             System.out.println("Please provide an operation code and 2 numeric values");
         }
+    }
+
+    static void executeInteractively() {
+        System.out.println("Enter an operation and two numbers:");
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+        String[] parts = userInput.split(" ");
+        performOperation(parts);
+    }
+
+    private static void performOperation(String[] parts) {
+        char opCode = opCodeFromString(parts[0]);
+        double leftVal = valueFromWord(parts[1]);
+        double rightVal = valueFromWord(parts[2]);
+        double result = execute(opCode, leftVal, rightVal);
+        System.out.println(result);
+
+
+
     }
 
     private static void handleCommandLine(String[] args) {
@@ -52,14 +75,14 @@ public class Main {
         return result;
     }
 
-    static char opCodeFromString(string operationName) {
+    static char opCodeFromString(String operationName) {
         char opCode = operationName.charAt(0);
         return opCode;
     }
 
-    double valueFromWord(String word) {
+    static double valueFromWord(String word) {
         String[] numberWords = {
-                "zero", "one", "two", "three", "four", "five", "six", "seven"
+                "zero", "one", "two", "three", "four", "five", "six", "seven",
                 "eight", "nine"
         };
         double value = 0d;
